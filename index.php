d880d8338be282c0cc93d20f0ae21e3ca3a5d01b
nemanjan00 <nemanjan00@gmail.com>
2015-11-03 09:36:18 +0100

    Document

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
 for($i = 0; $i < 100; $i++){
+	//execute it and do not wait :)
 	system("php test.php  > /dev/null 2>/dev/null &");
 }
--- test.php
+++ test.php
@@ -1,16 +1,17 @@
 <?php
+// Lets just read number from file
 $counter = file_get_contents("./counter.txt");
 
+//Increment it
 $counter = $counter + 1;
 
-echo $counter."\n";
-
+//Ok, now, let's do something slow, not like something in counter.txt can change while we are doing this. Or can it? 
 $n = Array(); 
 
 for($i = 0; $i < 1000; $i++){
 	$n[] = rand(1, 1000);
 }
 
-
+//Write new number
 file_put_contents("./counter.txt", $counter);
 
